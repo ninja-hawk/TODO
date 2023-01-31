5abf606c938bcca9e62c187262e8cbca488142cb
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-01 02:34:52 +0900

    [add]title template

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -4,6 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    titleTemplate: '%s - TODO',
     title: 'todo',
     meta: [
       { charset: 'utf-8' },
