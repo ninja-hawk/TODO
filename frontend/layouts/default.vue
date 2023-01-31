3b29841e85b9365b66679e109980769ec28b2b74
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-01 02:32:22 +0900

    [add]layout

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+<v-app>
+    <v-app-bar color="orange" app clipped-left>
+      <v-app-bar-nav-icon color="white" @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="white--text">TODO</v-toolbar-title>
+    </v-app-bar>
+
+  <v-app>
+    <div>layout</div>
+    <v-main>
+      <Nuxt />
+    </v-main>
+  </v-app>
+</v-app>
+</template>
