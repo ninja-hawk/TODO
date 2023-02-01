755033992b8a244cd3a661121e0039e3193b1d19
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-02 00:39:28 +0900

    [add]レスポンス対応インデックス画面

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,145 +1,46 @@
 <template>
-<v-app>
-  <v-container style="max-width: 500px">
-    <v-text-field
-      v-model="newTask"
-      label="What are you working on?"
-      solo
-      @keydown.enter="create"
-    >
-      <template #append>
-        <v-fade-transition>
-          <v-icon
-            v-if="newTask"
-            @click="create"
-          >
-            mdi-plus-circle
-          </v-icon>
-        </v-fade-transition>
-      </template>
-    </v-text-field>
-
-    <h2 class="text-h4 success--text pl-4">
-      Tasks:&nbsp;
-      <v-fade-transition leave-absolute>
-        <span :key="`tasks-${tasks.length}`">
-          {{ tasks.length }}
-        </span>
-      </v-fade-transition>
-    </h2>
-
-    <v-divider class="mt-4"></v-divider>
-
-    <v-row
-      class="my-1"
-      align="center"
-    >
-      <strong class="mx-4 info--text text--darken-2">
-        Remaining: {{ remainingTasks }}
-      </strong>
-
-      <v-divider vertical></v-divider>
-
-      <strong class="mx-4 success--text text--darken-2">
-        Completed: {{ completedTasks }}
-      </strong>
-
-      <v-spacer></v-spacer>
-
-      <v-progress-circular
-        :value="progress"
-        class="mr-2"
-      ></v-progress-circular>
-    </v-row>
-
-    <v-divider class="mb-4"></v-divider>
-
-    <v-card v-if="tasks.length > 0">
-      <v-slide-y-transition
-        class="py-0"
-        group
-        tag="v-list"
+<v-container fluid>
+  <v-row fill-height>
+      <v-col
+        cols="4"
+        md="4"
+        sm="12"
       >
-        <template v-for="(task, i) in tasks">
-          <v-divider
-            v-if="i !== 0"
-            :key="`${i}-divider`"
-          ></v-divider>
-
-          <v-list-item :key="`${i}-${task.text}`">
-            <v-list-item-action>
-              <v-checkbox
-                v-model="task.done"
-                :color="task.done && 'grey' || 'primary'"
-              >
-                <template #label>
-                  <div
-                    :class="task.done && 'grey--text' || 'primary--text'"
-                    class="ml-4"
-                    v-text="task.text"
-                  ></div>
-                </template>
-              </v-checkbox>
-            </v-list-item-action>
-
-            <v-spacer></v-spacer>
-
-            <v-scroll-x-transition>
-              <v-icon
-                v-if="task.done"
-                color="success"
-              >
-                mdi-check
-              </v-icon>
-            </v-scroll-x-transition>
-          </v-list-item>
-        </template>
-      </v-slide-y-transition>
-    </v-card>
+        <strong>Total Task</strong>
+        <Todo />
+      </v-col>
+      <v-col
+        cols="8"
+        md="8"
+        sm="12"
+      >
+        <v-row>
+          <v-col
+            v-for="j in 6"
+            :key="`${j}`"
+            cols="8"
+            :md="`${12/3}`"
+            :sm="`${12/3}`"
+          >
+            <strong>Task {{j}}</strong>
+            <Todo />
+          </v-col>
+        </v-row>
+      </v-col>
+  </v-row>
   </v-container>
-</v-app>
 </template>
 
 <script>
-  export default {
-  layout () {
-    return 'todo'
+import Todo from '~/components/Todo'
+
+export default {
+  components: {
+    Todo,
   },
-    data: () => ({
-      tasks: [
-        {
-          done: false,
-          text: 'Foobar',
-        },
-        {
-          done: false,
-          text: 'Fizzbuzz',
-        },
-      ],
-      newTask: null,
-    }),
 
-    computed: {
-      completedTasks () {
-        return this.tasks.filter(task => task.done).length
-      },
-      progress () {
-        return this.completedTasks / this.tasks.length * 100
-      },
-      remainingTasks () {
-        return this.tasks.length - this.completedTasks
-      },
-    },
-
-    methods: {
-      create () {
-        this.tasks.push({
-          done: false,
-          text: this.newTask,
-        })
-
-        this.newTask = null
-      },
-    },
-  }
+  data: () => ({
+     drawer: null
+  }),
+}
 </script>
