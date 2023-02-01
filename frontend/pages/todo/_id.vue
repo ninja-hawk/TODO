28c09c762ac59917eecd9e2c3948f2661b22b0f6
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-02 00:39:49 +0900

    [add]Task別の画面

---
+++ frontend/pages/todo/_id.vue
@@ -0,0 +1,26 @@
+<template>
+<v-container fluid>
+  <v-row>
+      <v-col
+        cols="12"
+      >
+        <strong>Task {{$route.params.id}}</strong>
+        <Todo />
+      </v-col>
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+import Todo from '~/components/Todo'
+
+export default {
+  components: {
+    Todo,
+  },
+
+  data: () => ({
+     drawer: null
+  }),
+}
+</script>
