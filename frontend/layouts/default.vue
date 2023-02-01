5ecbe30676ef8bb63ee8cbe09948f480f3201fb2
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-02 00:38:41 +0900

    [add]レイアウト初期設定

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,15 +1,50 @@
 <template>
-<v-app>
-    <v-app-bar color="orange" app clipped-left>
-      <v-app-bar-nav-icon color="white" @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="white--text">TODO</v-toolbar-title>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      class="pt-4"
+      color="grey lighten-3"
+      app
+      mini-variant
+      clipped
+      expand-on-hover
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey ${n === 2 ? 'darken' : 'lighten'}-1`"
+        :size="n === 2 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>TODO</v-toolbar-title>
+      <template #extension>
+        <v-tabs v-model="model" right>
+          <v-tab nuxt to="/">Total TODO</v-tab>
+          <v-tab
+            v-for = "i in 6"
+            :key="i"
+            nuxt
+            :to = "{path: `/todo/${i}`}"
+          >
+            Todo {{i}}
+          </v-tab>
+        </v-tabs>
+      </template>
     </v-app-bar>
 
-  <v-app>
-    <div>layout</div>
-    <v-main>
-      <Nuxt />
+    <v-main class="grey lighten-2">
+      <v-container>
+        <Nuxt />
+      </v-container>
     </v-main>
   </v-app>
-</v-app>
 </template>
+
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
+</script>
