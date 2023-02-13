ed747092206b0f9aed6bbf24309a3dd3d1723492
ninja-hawk <yanagi.taka.291@s.kyushu-u.ac.jp>
2023-02-14 05:39:40 +0900

    [update]title, shareの移動

--- frontend/store/layout.js
+++ frontend/store/layout.js
@@ -1,42 +1,21 @@
 export const state = () => ({
   drawer: false,
-  share: false,
-  title: "TODO"
 })
 
 export const mutations = {
   setDrawer: (state) => {
     state.drawer = !state.drawer
   },
-  setShare: (state) => {
-    state.share = !state.share
-  },
-  setTitle: (state, response) => {
-    state.title = response
-  }
 }
 
 export const getters = {
   drawer: (state) =>  {
     return state.drawer
   },
-  share: (state) =>  {
-    return state.share
-  },
-  title: (state) =>  {
-    return state.title
-  }
-
 }
 
 export const actions = {
   pushDrawer ({ commit }){
     commit('setDrawer')
   },
-  pushShare ({ commit }){
-    commit('setShare')
-  },
-  pushTitle ({commit}, argument){
-    commit('setTitle', argument)
-  }
 }
